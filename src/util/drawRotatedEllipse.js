739fb4d0f32d0d7f726ea49a48c67db3859514d8
sisobus <sisobus1@gmail.com>
2019-05-31 00:41:56 +0900

    Add a feature that draw rotatedEllipse(deprecated)

---
+++ src/util/drawRotatedEllipse.js
@@ -0,0 +1,19 @@
+import { drawRotatedEllipse } from './drawing.js';
+
+// http://stackoverflow.com/questions/2172798/how-to-draw-an-oval-in-html5-canvas
+
+/**
+ * @deprecated Use drawing.js:drawEllipse()
+ */
+export default function (context, x, y, w, h) {
+  const corner1 = {
+    x,
+    y
+  };
+  const corner2 = {
+    x: x + w,
+    y: y + h
+  };
+
+  drawRotatedEllipse(context, undefined, corner1, corner2, {}, 'canvas');
+}
