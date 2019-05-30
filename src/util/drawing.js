b132befeacacd39d0dd6a4eba8da35fd0af59cb8
sisobus <sisobus1@gmail.com>
2019-05-31 00:39:15 +0900

    Add a feature that draw rotatedEllipse

--- src/util/drawing.js
+++ src/util/drawing.js
@@ -1,7 +1,8 @@
 import external from '../externalModules.js';
-import { getDefault } from './getDefault.js'
+import { getDefault } from './getDefault.js';
 import { default as toolStyle } from '../stateManagement/toolStyle.js';
 import { default as textStyle } from '../stateManagement/textStyle.js';
+import { rotatePoint } from './pointProjector.js';
 
 /**
  * A {@link https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillStyle|color, gradient or pattern} to use inside shapes.
@@ -62,12 +63,14 @@ export function draw (context, fn) {
  * @param {ContextFn} fn - A drawing function to execute with the provided stroke pattern.
  */
 export function path (context, options, fn) {
-
   const { color, lineWidth, fillStyle, lineDash } = options;
 
   context.beginPath();
   context.strokeStyle = color || context.strokeStyle;
-  context.lineWidth = lineWidth || (lineWidth === undefined && toolStyle.getToolWidth()) || context.lineWidth;
+  context.lineWidth =
+    lineWidth ||
+    (lineWidth === undefined && toolStyle.getToolWidth()) ||
+    context.lineWidth;
   if (lineDash) {
     context.setLineDash(lineDash);
   }
@@ -117,7 +120,14 @@ export function setShadow (context, options) {
  *     system of the points passed in to the function. If "pixel" then cornerstone.pixelToCanvas
  *     is used to transform the points from pixel to canvas coordinates.
  */
-export function drawLine (context, element, start, end, options, coordSystem = 'pixel') {
+export function drawLine (
+  context,
+  element,
+  start,
+  end,
+  options,
+  coordSystem = 'pixel'
+) {
   path(context, options, (context) => {
     if (coordSystem === 'pixel') {
       const cornerstone = external.cornerstone;
@@ -143,7 +153,13 @@ export function drawLine (context, element, start, end, options, coordSystem = '
  *     system of the points passed in to the function. If "pixel" then cornerstone.pixelToCanvas
  *     is used to transform the points from pixel to canvas coordinates.
  */
-export function drawLines (context, element, lines, options, coordSystem = 'pixel') {
+export function drawLines (
+  context,
+  element,
+  lines,
+  options,
+  coordSystem = 'pixel'
+) {
   path(context, options, (context) => {
     lines.forEach((line) => {
       let start = line.start;
@@ -174,7 +190,14 @@ export function drawLines (context, element, lines, options, coordSystem = 'pixe
  *     system of the points passed in to the function. If "pixel" then cornerstone.pixelToCanvas
  *     is used to transform the points from pixel to canvas coordinates.
  */
-export function drawJoinedLines (context, element, start, points, options, coordSystem = 'pixel') {
+export function drawJoinedLines (
+  context,
+  element,
+  start,
+  points,
+  options,
+  coordSystem = 'pixel'
+) {
   path(context, options, (context) => {
     if (coordSystem === 'pixel') {
       const cornerstone = external.cornerstone;
@@ -201,13 +224,109 @@ export function drawJoinedLines (context, element, start, points, options, coord
  *     system of the points passed in to the function. If "pixel" then cornerstone.pixelToCanvas
  *     is used to transform the points from pixel to canvas coordinates.
  */
-export function drawCircle (context, element, center, radius, options, coordSystem = 'pixel') {
+export function drawCircle (
+  context,
+  element,
+  center,
+  radius,
+  options,
+  coordSystem = 'pixel'
+) {
   if (coordSystem === 'pixel') {
     center = external.cornerstone.pixelToCanvas(element, center);
   }
 
   path(context, options, (context) => {
     context.arc(center.x, center.y, radius, 0, 2 * Math.PI);
+  });
+}
+
+/**
+ * Draw an ellipse within the bounding box defined by `corner1` and `corner2`.
+ * @public
+ * @method drawRotatedEllipse
+ * @memberof Drawing
+ *
+ * @param {CanvasRenderingContext2D} context - Target context
+ * @param {HTMLElement} element - The DOM Element to draw on
+ * @param {Object} corner1 - `{ x, y }` in either pixel or canvas coordinates.
+ * @param {Object} corner2 - `{ x, y }` in either pixel or canvas coordinates.
+ * @param {Object} corner3 - `{ x, y }` in either pixel or canvas coordinates.
+ * @param {Object} options - See {@link path}
+ * @param {String} [coordSystem='pixel'] - Can be "pixel" (default) or "canvas". The coordinate
+ *     system of the points passed in to the function. If "pixel" then cornerstone.pixelToCanvas
+ *     is used to transform the points from pixel to canvas coordinates.
+ * @param {Number} initialRotation - Ellipse initial rotation
+ * @returns {undefined}
+ */
+export function drawRotatedEllipse (
+  context,
+  element,
+  corner1,
+  corner2,
+  corner3,
+  options,
+  coordSystem = 'pixel',
+  initialRotation = 0.0
+) {
+  const { isFirst } = corner3;
+
+  if (coordSystem === 'pixel') {
+    corner1 = external.cornerstone.pixelToCanvas(element, corner1);
+    corner2 = external.cornerstone.pixelToCanvas(element, corner2);
+    corner3 = external.cornerstone.pixelToCanvas(element, corner3);
+  }
+
+  const viewport = external.cornerstone.getViewport(element);
+
+  // Calculate the center of the image
+  const { clientWidth: width, clientHeight: height } = element;
+  const { scale, translation } = viewport;
+  const rotation = viewport.rotation - initialRotation;
+
+  const centerPoint = {
+    x: width / 2 + translation.x * scale,
+    y: height / 2 + translation.y * scale
+  };
+
+  if (Math.abs(rotation) > 0.05) {
+    corner1 = rotatePoint(corner1, centerPoint, -rotation);
+    corner2 = rotatePoint(corner2, centerPoint, -rotation);
+    corner3 = external.cornerstone.pixelToCanvas(element, corner3);
+  }
+  const w = Math.abs(corner1.x - corner2.x);
+  const h = Math.abs(corner1.y - corner2.y);
+  const xMin = Math.min(corner1.x, corner2.x);
+  const yMin = Math.min(corner1.y, corner2.y);
+
+  let center = {
+    x: xMin + w / 2,
+    y: yMin + h / 2
+  };
+
+  if (Math.abs(rotation) > 0.05) {
+    center = rotatePoint(center, centerPoint, rotation);
+  }
+  const square = (x) => x * x;
+  const longestDistance = Math.sqrt(square(w) + square(h));
+  const shortestDistance = isFirst
+    ? 0
+    : Math.sqrt(square(corner3.x - center.x) + square(corner3.y - center.y));
+
+  const angle =
+    (rotation * Math.PI) / 180 +
+    Math.atan2(corner2.y - corner1.y, corner2.x - corner1.x);
+
+  path(context, options, (context) => {
+    context.ellipse(
+      center.x,
+      center.y,
+      longestDistance / 2,
+      shortestDistance,
+      angle,
+      0,
+      Math.PI * 2
+    );
   });
 }
 
@@ -223,7 +342,14 @@ export function drawCircle (context, element, center, radius, options, coordSyst
  *     system of the points passed in to the function. If "pixel" then cornerstone.pixelToCanvas
  *     is used to transform the points from pixel to canvas coordinates.
  */
-export function drawEllipse (context, element, corner1, corner2, options, coordSystem = 'pixel') {
+export function drawEllipse (
+  context,
+  element,
+  corner1,
+  corner2,
+  options,
+  coordSystem = 'pixel'
+) {
   // http://stackoverflow.com/questions/2172798/how-to-draw-an-oval-in-html5-canvas
   if (coordSystem === 'pixel') {
     corner1 = external.cornerstone.pixelToCanvas(element, corner1);
@@ -264,7 +390,14 @@ export function drawEllipse (context, element, corner1, corner2, options, coordS
  *     system of the points passed in to the function. If "pixel" then cornerstone.pixelToCanvas
  *     is used to transform the points from pixel to canvas coordinates.
  */
-export function drawRect (context, element, corner1, corner2, options, coordSystem = 'pixel') {
+export function drawRect (
+  context,
+  element,
+  corner1,
+  corner2,
+  options,
+  coordSystem = 'pixel'
+) {
   if (coordSystem === 'pixel') {
     const cornerstone = external.cornerstone;
 
@@ -294,7 +427,14 @@ export function drawRect (context, element, corner1, corner2, options, coordSyst
  *     system of the points passed in to the function. If "pixel" then cornerstone.pixelToCanvas
  *     is used to transform the points from pixel to canvas coordinates.
  */
-export function fillOutsideRect (context, element, corner1, corner2, options, coordSystem = 'pixel') {
+export function fillOutsideRect (
+  context,
+  element,
+  corner1,
+  corner2,
+  options,
+  coordSystem = 'pixel'
+) {
   if (coordSystem === 'pixel') {
     const cornerstone = external.cornerstone;
 
@@ -313,7 +453,6 @@ export function fillOutsideRect (context, element, corner1, corner2, options, co
   });
 }
 
-
 /**
  * Draw a filled rectangle defined by `boundingBox` using the style defined by `fillStyle`.
  *
@@ -323,7 +462,12 @@ export function fillOutsideRect (context, element, corner1, corner2, options, co
  */
 export function fillBox (context, boundingBox, fillStyle) {
   context.fillStyle = fillStyle;
-  context.fillRect(boundingBox.left, boundingBox.top, boundingBox.width, boundingBox.height);
+  context.fillRect(
+    boundingBox.left,
+    boundingBox.top,
+    boundingBox.width,
+    boundingBox.height
+  );
 }
 
 /**
@@ -335,13 +479,23 @@ export function fillBox (context, boundingBox, fillStyle) {
  * @param {FillStyle} fillStyle - The fillStyle to apply to the text.
  * @param {Number} padding - The amount of padding above/below each line in canvas units. Note this gives an inter-line spacing of `2*padding`.
  */
-export function fillTextLines (context, boundingBox, textLines, fillStyle, padding) {
+export function fillTextLines (
+  context,
+  boundingBox,
+  textLines,
+  fillStyle,
+  padding
+) {
   const fontSize = textStyle.getFontSize();
 
   context.font = textStyle.getFont();
   context.textBaseline = 'top';
   context.fillStyle = fillStyle;
   textLines.forEach(function (text, index) {
-    context.fillText(text, boundingBox.left + padding, boundingBox.top + padding + index * (fontSize + padding));
+    context.fillText(
+      text,
+      boundingBox.left + padding,
+      boundingBox.top + padding + index * (fontSize + padding)
+    );
   });
 }
